daa9b9d880829fbeef2bf734fb6326d0e3bbd125
unknown <lequangphuoc2305@gmail.com>
2023-07-04 23:14:13 +0700

    [Queen store][Le Quang Phuoc][Update Model]

--- queen_store/src/main/java/com/example/queen_store/model/voucher/Voucher.java
+++ queen_store/src/main/java/com/example/queen_store/model/voucher/Voucher.java
@@ -3,17 +3,17 @@ package com.example.queen_store.model.voucher;
 import java.util.Objects;
 
 public class Voucher {
-    private int voucher_id;
-    private String voucher_name;
-    private float voucher_rate;
+    private int id;
+    private String name;
+    private float rate;
 
     public Voucher() {
     }
 
-    public Voucher(int voucher_id, String voucher_name, float voucher_rate) {
-        this.voucher_id = voucher_id;
-        this.voucher_name = voucher_name;
-        this.voucher_rate = voucher_rate;
+    public Voucher(int id, String name, float rate) {
+        this.id = id;
+        this.name = name;
+        this.rate = rate;
     }
 
     @Override
@@ -21,35 +21,35 @@ public class Voucher {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Voucher voucher = (Voucher) o;
-        return voucher_id == voucher.voucher_id;
+        return id == voucher.id;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(voucher_id);
+        return Objects.hash(id);
     }
 
-    public int getVoucher_id() {
-        return voucher_id;
+    public int getId() {
+        return id;
     }
 
-    public void setVoucher_id(int voucher_id) {
-        this.voucher_id = voucher_id;
+    public void setId(int id) {
+        this.id = id;
     }
 
-    public String getVoucher_name() {
-        return voucher_name;
+    public String getName() {
+        return name;
     }
 
-    public void setVoucher_name(String voucher_name) {
-        this.voucher_name = voucher_name;
+    public void setName(String name) {
+        this.name = name;
     }
 
-    public float getVoucher_rate() {
-        return voucher_rate;
+    public float getRate() {
+        return rate;
     }
 
-    public void setVoucher_rate(float voucher_rate) {
-        this.voucher_rate = voucher_rate;
+    public void setRate(float rate) {
+        this.rate = rate;
     }
 }
